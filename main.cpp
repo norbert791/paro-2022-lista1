44ae0f12a1de7569434c15fa1fa0ec678a2e2d3e
Norbert <norbert.jasniewicz@protonmail.com>
2022-03-12 20:48:08 +0100

    Dodanie pliki projektu
    -Błąd wskazany przez Clang-Tidy znajduje się w linii 9 (Clang-Tidy: user nullptr)

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+
+int main(int N, char** args) {
+    if (N < 2) {
+        std::cout<<"You must input an integer."<<std::endl;
+        return 0;
+    }
+    long int age = std::strtol(args[1],NULL, 10);
+    if (age >= 18) {
+        std::cout<<"Happy adult life!"<<std::endl;
+    }
+    else if (age > 0 && age < 17) {
+        std::cout<<"Happy adolescence!"<<std::endl;
+    }
+    else {
+        std::cout<<"Happy birthdate-awaiting!"<<std::endl;
+    }
+    return 0;
+}
