85551e9a4a0bf10b4da506ed1eca5d1f5ac77068
Norbert <norbert.jasniewicz@protonmail.com>
2022-03-12 20:55:25 +0100

    Poprawiono błąd wskazany przez Clang-Tidy

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main(int N, char** args) {
         std::cout<<"You must input an integer."<<std::endl;
         return 0;
     }
-    long int age = std::strtol(args[1],NULL, 10);
+    long int age = std::strtol(args[1],nullptr, 10);
     if (age >= 18) {
         std::cout<<"Happy adult life!"<<std::endl;
     }
